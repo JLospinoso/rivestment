9201835c38a8471e1d9f82c87e68efb828e2a873
Josh Lospinoso <josh@lospi.net>
2017-04-12 16:43:38 -0500

    CSS minification

--- Rivestment/Gulpfile.js
+++ Rivestment/Gulpfile.js
@@ -5,6 +5,7 @@ const minifyCSS = require('gulp-cssnano');
 const mocha = require('gulp-mocha');
 const browserify = require('gulp-browserify');
 const del = require('del');
+const concatCss = require('gulp-concat-css');
 
 gulp.task('clean', function(){
   return del(['web/dist'])
@@ -33,43 +34,29 @@ gulp.task('images', function(){
 
 gulp.task('css', function(){
   return gulp.src([
-      'web/www/css/*.css',
+      'web/www/css/rivestment.css',
       'node_modules/metrics-graphics/dist/metricsgraphics.css',
       'node_modules/bootstrap/dist/css/bootstrap.css'
     ])
+    .pipe(concatCss("bundle.css"))
     .pipe(minifyCSS())
     .pipe(gulp.dest('web/dist/www/css'));
 });
 
 gulp.task('react', function(){
-  return gulp.src([
-      'web/www/js/*.jsx'])
+  return gulp.src(['web/www/js/*.jsx'])
     .pipe(babel({
       plugins: ['transform-react-jsx'],
       presets: ['es2015','stage-2']
     }))
     .pipe(browserify())
-//    .pipe(uglify())
-    .pipe(gulp.dest('web/dist/www/js'));
-});
-
-gulp.task('scripts', function(){
-  return gulp.src([
-      'web/www/js/*.js',
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/bootstrap/dist/js/bootstrap.js',
-      'node_modules/react/dist/react.js',
-      'node_modules/react-dom/dist/react-dom.js'])
-//    .pipe(uglify())
+    .pipe(uglify())
     .pipe(gulp.dest('web/dist/www/js'));
 });
 
 gulp.task('default',
   gulp.series(
     'clean',
-    gulp.series(
-      gulp.parallel('sites', 'images', 'css', 'react'),
-      'scripts'
-    )
+    gulp.parallel('sites', 'images', 'css', 'react')
   )
 );
